0dbaef49baa6c66ffb0922ea72ef2e409ba917bd
Jose <jrdjhc45@gmail.com>
2020-05-15 13:49:45 -0600

    creacion del menu con las tres opciones del juego

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#include <stdlib.h>
+#include <time.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int opcion = 0;
+	while(opcion != 3){
+		cout<<"Bienvenido al Juego de la vida de Conway "<<endl<<"1.Jugar con Matriz Random "<<endl<<"2.Jugar con Matriz Predefinida "<<endl<<"3.Salir "
+		<<endl<<"Ingrese la opcion a ejecutar: ";
+		
+	}
+	
+	
+	return 0;
+}
